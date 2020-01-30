e25f0adcb8cd9233cffb5eea5d2a09ebdf5dc20e
coruscar <jonathan22711@gmail.com>
2020-01-29 22:49:04 -0800

    mona is looking pretty weird

---
+++ pillowtest.py
@@ -0,0 +1,46 @@
+from PIL import Image
+import os 
+
+img = Image.open('Mona1.jpg')
+
+img_bytes = img.tobytes()
+
+print(img_bytes[1])
+
+bytelist = []
+
+for byte in img_bytes:
+    if byte > 200:
+        byte = 50
+    else:
+        byte = 245
+    bytelist.append(byte)
+
+img_bytes_new = bytes(bytearray(bytelist))
+
+print(img_bytes[1])
+
+
+# img = Image.frombytes(img.mode, img.size ,img_bytes)
+img2 = Image.frombytes(img.mode, img.size ,img_bytes_new)
+
+# img.show()
+img2.show()
+print("type(img_bytes) = " + str(type(img_bytes)))
+
+
+## Interesting stuff
+
+# img_b = img.convert(mode='L').show()
+
+
+# bytelist = []
+
+# for byte in bytelist:
+#     if byte > 155:
+#         byte = 255
+#     else:
+#         byte = 0
+#     bytelist.append(byte)
+
+# img_bytes = bytes(bytelist)
